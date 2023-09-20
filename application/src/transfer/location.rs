dc68830ce099835305044343fb86487626d14166
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-09-20 19:56:45 +0900

    🎨 - Changed location dto.

--- application/src/transfer/location.rs
+++ application/src/transfer/location.rs
@@ -12,44 +12,38 @@ pub struct LocationDto {
 
 impl From<Location> for LocationDto {
     fn from(value: Location) -> Self {
-        let DestructLocation {
-            id,
-            pos,
-            localize
-        } = value.into_destruct();
-        let DestructPosition {
-            x, y
-        } = pos.into_destruct();
+        let DestructLocation { id, pos, localize } = value.into_destruct();
+        let DestructPosition { x, y } = pos.into_destruct();
         Self {
             id: id.into(),
             latitude: y.into(),
             longitude: x.into(),
-            localize: localize.into_iter()
-                .map(Into::into)
-                .collect()
+            localize: localize
+                .into_iter()
+                .map(|loc| loc.into_destruct())
+                .map(|des| (des.country_code.into(), des.localize))
+                .collect(),
         }
     }
 }
-
 
 #[derive(Debug)]
 pub struct CreateLocationDto {
     pub latitude: f64,
     pub longitude: f64,
-    pub localize: Vec<(String, String)>
+    pub localize: Vec<(String, String)>,
 }
-
 
 #[derive(Debug)]
 pub struct UpdateLocationDto {
     pub id: Uuid,
     pub latitude: f64,
     pub longitude: f64,
-    pub localize: Vec<(String, String)>
+    pub localize: Vec<(String, String)>,
 }
-
 
 #[derive(Debug)]
 pub struct DeleteLocationDto {
-   pub id: Uuid
+    pub id: Uuid,
+    pub localize: Option<String>,
 }
