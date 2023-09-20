2b6812eb661ba0f5c1bed771b1b453040f2d5e62
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-09-20 20:13:19 +0900

    🔨 - Added route location for AppRouter.

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,4 +1,6 @@
 pub mod error;
+mod extensions;
 mod handler;
 pub mod routes;
 
+pub use self::handler::*;
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,31 +1,45 @@
-use std::net::SocketAddr;
 use axum::{Router, Server};
+use server::{Handler, routes};
+use std::net::SocketAddr;
+use axum::routing::get;
 use tower_http::trace::TraceLayer;
-use tracing_subscriber::Layer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::Layer;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let appender = tracing_appender::rolling::daily(std::path::Path::new("./logs/"), "debug.log");
     let (non_blocking_appender, _guard) = tracing_appender::non_blocking(appender);
     tracing_subscriber::registry()
-        .with(tracing_subscriber::fmt::layer()
-            .with_filter(tracing_subscriber::EnvFilter::new(std::env::var("RUST_LOG")
-                .unwrap_or_else(|_| "driver=debug,server=debug,tower_http=debug,hyper=debug,sqlx=debug".into())))
-            .with_filter(tracing_subscriber::filter::LevelFilter::DEBUG))
-        .with(tracing_subscriber::fmt::Layer::default()
-            .with_writer(non_blocking_appender)
-            .with_ansi(false)
-            .with_filter(tracing_subscriber::filter::LevelFilter::DEBUG))
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_filter(tracing_subscriber::EnvFilter::new(
+                    std::env::var("RUST_LOG").unwrap_or_else(|_| {
+                        "driver=debug,server=debug,tower_http=debug,hyper=debug,sqlx=debug".into()
+                    }),
+                ))
+                .with_filter(tracing_subscriber::filter::LevelFilter::DEBUG),
+        )
+        .with(
+            tracing_subscriber::fmt::Layer::default()
+                .with_writer(non_blocking_appender)
+                .with_ansi(false)
+                .with_filter(tracing_subscriber::filter::LevelFilter::DEBUG),
+        )
         .init();
 
+    let handler = Handler::init().await?;
+
     let app = Router::new()
-        .layer(TraceLayer::new_for_http());
+        .route("/locations", get(routes::locations))
+        .layer(TraceLayer::new_for_http())
+        .with_state(handler);
 
     let bind = SocketAddr::from(([0, 0, 0, 0], 3854));
 
-    tracing::info!("Stellar Starting...");
+    tracing::info!("WAWWD API server Starting...");
+    tracing::info!("Server listening on {}", bind.to_string());
 
     Server::bind(&bind)
         .serve(app.into_make_service())
