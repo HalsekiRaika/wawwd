5e39176ed46352eb1f575b4bcafc78766cabb30a
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-10-10 15:39:47 +0900

    🔥 - Delete `GeoJson` extension.

--- server/src/extensions/geojson.rs
+++
@@ -1,9 +0,0 @@
-use axum::response::{IntoResponse, Response};
-
-pub struct GeoJson<T>(T);
-
-impl<T> IntoResponse for GeoJson<T> {
-    fn into_response(self) -> Response {
-        todo!()
-    }
-}
