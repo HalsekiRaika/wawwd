90c700fa3995f43a5c191db9c8397d412336e524
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-10-10 15:43:11 +0900

    🥅 - Revise the wording of an error sentence.

--- server/src/error.rs
+++ server/src/error.rs
@@ -1,42 +1,47 @@
+use application::error::ApplicationError;
 use axum::http::StatusCode;
-use axum::Json;
 use axum::response::{IntoResponse, Response};
-use serde_json::json;
+use axum::Json;
 use driver::error::DriverError;
 use kernel::error::KernelError;
+use serde_json::json;
 
 #[derive(Debug, thiserror::Error)]
 pub enum ServerError {
     #[error(transparent)]
+    IO(anyhow::Error),
+    #[error(transparent)]
     HandlerInitialization(anyhow::Error),
+    #[error(transparent)]
+    Application(anyhow::Error),
     #[error(transparent)]
     Driver(anyhow::Error),
     #[error(transparent)]
     Kernel(anyhow::Error),
-    #[error("Failed required environment variable not set! `{0}` should set.")]
+    #[error("Required environment variable not set! `{0}` must set.")]
     EnvError(&'static str),
 }
 
 impl IntoResponse for ServerError {
     fn into_response(self) -> Response {
         let e = match self {
-            ServerError::HandlerInitialization(e) => {
-                e.to_string()
-            }
-            ServerError::Driver(e) => {
-                e.to_string()
-            }
-            ServerError::Kernel(e) => {
-                e.to_string()
-            }
-            ServerError::EnvError(e) => {
-                e.to_string()
-            }
+            ServerError::IO(e) => e.to_string(),
+            ServerError::HandlerInitialization(e) => e.to_string(),
+            ServerError::Driver(e) => e.to_string(),
+            ServerError::Application(e) => e.to_string(),
+            ServerError::Kernel(e) => e.to_string(),
+            ServerError::EnvError(e) => e.to_string(),
         };
 
         let json = json!({ "error": e });
 
         (StatusCode::BAD_REQUEST, Json(json)).into_response()
+    }
+}
+
+impl From<ApplicationError> for ServerError {
+    fn from(value: ApplicationError) -> Self {
+        Self::Application(anyhow::Error::new(value))
     }
 }
 
