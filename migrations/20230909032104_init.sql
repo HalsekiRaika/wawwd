d8d35369b2a6f1b8d2b247ff4613fad7c0005c3c
ReiRokusanami <reirokusanami.rdh@gmail.com>
2023-10-13 09:57:49 +0900

    🔧 - Added table `rings`, `instances`.

--- migrations/20230909032104_init.sql
+++ migrations/20230909032104_init.sql
@@ -11,3 +11,25 @@ CREATE TABLE location_mark_localized_name(
   PRIMARY KEY (id, country),
   FOREIGN KEY (id) REFERENCES location_mark(id) ON DELETE CASCADE
 );
+
+CREATE TABLE instances(
+  id          UUID        NOT NULL PRIMARY KEY,
+  location    UUID        NOT NULL,
+  laps        SERIAL      NOT NULL,
+  started_at  TIMESTAMPTZ NOT NULL DEFAULT clock_timestamp(),
+  finished_at TIMESTAMPTZ,
+
+  FOREIGN KEY (location) REFERENCES location_mark(id) ON DELETE CASCADE
+);
+
+CREATE TABLE rings(
+  id         UUID NOT NULL PRIMARY KEY ,
+  instance   UUID NOT NULL,
+  pos_in     GEOGRAPHY(POINT, 4326) NOT NULL,
+  hue        SERIAL NOT NULL,
+  addr       INET NOT NULL,
+  index      SERIAL NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT clock_timestamp(),
+
+  FOREIGN KEY (instance) REFERENCES instances(id) ON DELETE CASCADE
+);
