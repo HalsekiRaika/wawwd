289d5ce414327641a1928cee4adb6b053e69a54d
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2023-10-24 20:11:20 +0900

    🎨 - Changed file line encoding `CRLF` to `LF`

--- kernel/src/entities.rs
+++ kernel/src/entities.rs
@@ -2,3 +2,4 @@ pub mod geology;
 pub mod instance;
 pub mod location;
 pub mod ring;
+pub mod token;
--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod entities;
 pub mod error;
 pub mod repository;
+pub mod security;
 
 #[cfg(feature = "time")]
 #[cfg(feature = "uuid")]
